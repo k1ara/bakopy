13ef5f854323aa5089d4bd54f10933efb48c8599
k1ara <lynmaru@gmail.com>
2017-07-21 11:46:16 -0500

    copymov copia y cobra directorio actual

--- cop&mov.bat
+++ cop&mov.bat
@@ -1,2 +1,12 @@
-::copiar archivos a destino remoto
-::mover archivos a carpeta distinta y dejar limpia el local
+:-copiar archivos a destino remoto
+:-mover archivos a carpeta distinta
+:-borrar carpeta local
+
+cd prueba1
+
+robocopy directorio1 directorio2 *.txt /E /V /FP /R:2 /W:10 /TS /BYTES
+
+cd directorio1
+
+del *.txt
+
